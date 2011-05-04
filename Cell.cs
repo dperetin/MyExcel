0b0c614fa96ebc920a814690a452d06e79604ede
petrap <petrap.zg@gmail.com>
2011-05-04 18:40:05 +0200

    za sve oznake stupaca sam zamijenila string sa int-om

--- Cell.cs
+++ Cell.cs
@@ -8,15 +8,15 @@ namespace MyExcel
     class Cell
     {
         private int red;
-        private string stupac;
+        private int stupac;
         private string sadrzaj;
 
-        private Cell(string s, int r)
+        private Cell(int s, int r)
         {
             red = r;
             stupac = s;
         }
-        public static Cell NapraviCeliju(string s, int r)
+        public static Cell NapraviCeliju(int s, int r)
         {
             return new Cell(s, r);
         }
@@ -24,11 +24,11 @@ namespace MyExcel
 
     class Celije
     {
-        Dictionary<KeyValuePair<string, int>, Cell> sveCelije = new Dictionary<KeyValuePair<string,int>,Cell>();
+        Dictionary<KeyValuePair<int, int>, Cell> sveCelije = new Dictionary<KeyValuePair<int, int>,Cell>();
 
-        public void Dodaj(string s, int r)
+        public void Dodaj(int s, int r)
         {
-            KeyValuePair<string, int> index = new KeyValuePair<string, int>(s, r);
+            KeyValuePair<int, int> index = new KeyValuePair<int, int>(s, r);
             sveCelije.Add(index, Cell.NapraviCeliju(s, r));
         }
     }
