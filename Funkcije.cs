0dc9754b00eab848ef8fd74b2e39a6b41d22faa9
dperetin <dperetin@gmail.com>
2011-06-25 11:35:07 +0200

    Dodani opisi formula

--- Funkcije.cs
+++ Funkcije.cs
@@ -32,8 +32,48 @@ namespace MyExcel
             SveFunkcije.Add("floor", new izracunaj(Floor));
             SveFunkcije.Add("count", new izracunaj(Count));
 
-            opisi.Add("min", "Minimum\n\n\tKorištenje:\nMIN(x1; x2; ...; xn) ili \n\tMIN(x1: xn)");
-           
+            opisi.Add("min", "Minimum\n\n" +
+                             "    Korištenje:\n    MIN ( x1; x2; ...; xn ) \n    MIN ( x1: xn )\n\n" + 
+                             "    Opis:\n    Vraća minimum vrijednosti x1 ... xn");
+            opisi.Add("max", "Maksimum\n\n" +
+                             "    Korištenje:\n    MAX ( x1; x2; ...; xn )  \n    MAX ( x1: xn )\n\n" +
+                             "    Opis:\n    Vraća maksimum vrijednosti x1 ... xn");
+            opisi.Add("sum", "Suma\n\n" + 
+                             "    Korištenje:\n    SUM ( x1; x2; ...; xn )  \n    SUM ( x1: xn )\n\n" +
+                             "    Opis:\n    Vraća sumu vrijednosti x1 ... xn");
+            opisi.Add("average", "Aritmeticka sredina\n\n" + 
+                             "    Korištenje:\n    AVERAGE ( x1; x2; ...; xn ) ili \n    AVERAGE ( x1: xn )\n\n" + 
+                             "    Opis:\n    Vraća aritmetičku sredinu vrijednosti x1 ... xn");
+            opisi.Add("log", "Logaritam\n\n" + 
+                             "    Korištenje:\n    LOG ( x )  \n    LOG ( x; b )\n\n" +
+                             "    Opis:\n    Vraća logaritam broja x po bazi b (Default b = 10)");
+            opisi.Add("mod", "Modulo\n\n" + 
+                             "    Korištenje:\n    MOD ( a; b ) \n\n"+
+                             "    Opis:\n    Vraća ostatak dijeljenja a sa b");
+            opisi.Add("sin", "Sinus\n\n" + 
+                             "    Korištenje:\n    SIN ( x ) \n\n" + 
+                             "    Opis:\n    Vraća sinus broja x");
+            opisi.Add("cos", "Kosinus\n\n" + 
+                             "    Korištenje:\n    COS ( x ) \n\n" +
+                             "    Opis:\n    Vraća kosinus broja x");
+            opisi.Add("tan", "Tangens\n\n" + 
+                             "    Korištenje:\n    TAN ( x ) \n\n" +
+                             "    Opis:\n    Vraća tangens broja x");
+            opisi.Add("abs", "Apsolutno\n\n" + 
+                             "    Korištenje:\n    ABS ( x ) \n\n" +
+                             "    Opis:\n    Vraća apsolutnu vrijednost broja x");
+            opisi.Add("sqrt", "Apsolutno\n\n" + 
+                             "    Korištenje:\n    SQRT ( x ) \n\n" +
+                             "    Opis:\n    Vraća drugi korjen broja x");
+            opisi.Add("ceil", "Najmanje cijelo\n\n" + 
+                              "    Korištenje:\n    CEIL ( x ) \n\n" +
+                              "    Opis:\n    Vraća najmanji cijeli broj veći od broja x");
+            opisi.Add("floor", "Najveće cijelo\n\n" + 
+                               "    Korištenje:\n    FLOOR ( x ) \n\n" +
+                               "    Opis:\n    Vraća najveći cijeli broj manji od broja x");
+            opisi.Add("count", "Brojanje\n\n" + 
+                               "    Korištenje:\n    COUNT ( v; x1; ...; xn ) \n\n" +
+                               "    Opis:\n    Vraća koliko od vrijednosti x1 ... xn je jednako v");
         }
 
         private static double Average(List<double> celije)
@@ -126,6 +166,8 @@ namespace MyExcel
         }
         private static double Logaritam(List<double> celije)
         {
+            if (celije.Count == 0)
+                return 0;
             double baza = 10;
             if (celije.Count > 1)
             {
@@ -137,6 +179,8 @@ namespace MyExcel
         }
         private static double Mod(List<double> celije)
         {
+            if (celije.Count != 2)
+                return 0;
             return celije[1] % celije[0];
         }
         private static double Ceil(List<double> celije)
